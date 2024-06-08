a097ab8c6e635dc12190c5bc8485b16cd011924e
gfourie23 <135474497+gfourie23@users.noreply.github.com>
2024-06-08 12:47:32 -0600

    Update models.py

--- models.py
+++ models.py
@@ -46,6 +46,11 @@ class Patient(db.Model):
         db.Text,
     )
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+
 
 def connect_db(app):
     """Connect this database to Flask app.
