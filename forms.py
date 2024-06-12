a0a84e16390fff5986f6c468925792e2ceeed104
gfourie23 <135474497+gfourie23@users.noreply.github.com>
2024-06-12 14:20:40 -0600

    Update forms.py

--- forms.py
+++ forms.py
@@ -12,6 +12,10 @@ class NewPatientForm(FlaskForm):
 
     city = StringField('City', validators=[DataRequired()])
 
+    email = StringField('Email', validators=[DataRequired()])
+
+    phone_number = StringField('Phone Number', validators=[DataRequired()])
+
     frequency = StringField('Weekly Frequency', validators=[DataRequired()])
 
     timeframe_start = StringField('Start Date', validators=[DataRequired()])
@@ -23,13 +27,17 @@ class CheckboxField(SelectMultipleField):
     option_widget = widgets.CheckboxInput()
 
 class PatientEditForm(FlaskForm):
-    """Form to edit patient preferences."""
+    """Form to edit patient information and preferences."""
 
     name = StringField('Name', validators=[DataRequired()])
 
     address = StringField('Address', validators=[DataRequired()])
 
     city = StringField('City', validators=[DataRequired()])
+
+    email = StringField('Email', validators=[DataRequired()])
+
+    phone_number = StringField('Phone Number', validators=[DataRequired()])
 
     frequency = StringField('Weekly Frequency', validators=[DataRequired()])
 
@@ -40,25 +48,3 @@ class PatientEditForm(FlaskForm):
     preferred_days = CheckboxField('Preferred Days:', choices=["Monday", "Tuesday", "Wednesday", "Thurdsay", "Friday"])
 
     preferred_times = CheckboxField("Preferred Times:", choices=["9am-11am", "11am-1pm", "1pm-3pm", "3pm-5pm"])
-
-
-
-
-         
-"""class UserForm(FlaskForm):
-    Edit user address and availability.
-    days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-
-    for day in days_of_week:
-        setattr(
-            UserForm,
-            f'{day.lower()}_start_time',
-            SelectField(f'{day} - Start Time', choices=[(f'{hour:02}:00', f'{hour:02}:00') for hour in range(7, 13)])
-        )
-        setattr(
-            UserForm,
-            f'{day.lower()}_end_time',
-            SelectField(f'{day} - End Time', choices=[(f'{hour:02}:00', f'{hour:02}:00') for hour in range(12, 19)])
-        )"""
-
-#class DeleteForm(FlaskForm):    
